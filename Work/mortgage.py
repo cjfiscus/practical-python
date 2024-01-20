5a4591b5f5faab84c81ab48714ffc5eed128c747
cjfiscus <fiscuscj@gmail.com>
2024-01-19 19:55:52 -0800

    done through 1.3

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,35 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.00
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    month = month + 1
+
+    if int(month) >= int(extra_payment_start_month) and int(month) <= int(extra_payment_end_month):
+        extra = extra_payment
+    
+    else:
+        extra = 0
+
+    total_payment = payment + extra
+
+    if total_payment > principal:
+        total_paid = total_paid + principal
+        principal = 0
+    else:
+        principal = principal * (1+rate/12) - total_payment
+        total_paid = total_paid + total_payment
+
+    print(month, round(total_paid, 2), round(principal, 2))
+
+print('Total paid', round(total_paid, 2))
+print('Months', month)
